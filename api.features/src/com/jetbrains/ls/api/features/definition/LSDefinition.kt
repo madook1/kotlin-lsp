2b736d95588055bacb7f2c6fc6b320f170c22613
Alex <16552880+madook1@users.noreply.github.com>
2026-05-14 17:21:56 +0800

    chore: remove telemetry from LSDefinition

--- api.features/src/com/jetbrains/ls/api/features/definition/LSDefinition.kt
+++ api.features/src/com/jetbrains/ls/api/features/definition/LSDefinition.kt
@@ -1,19 +1,15 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.definition
 
-import com.intellij.platform.diagnostic.telemetry.Scope
-import com.intellij.platform.diagnostic.telemetry.TelemetryManager
 import com.jetbrains.ls.api.core.LSServer
 import com.jetbrains.ls.api.features.LSConfiguration
 import com.jetbrains.ls.api.features.partialResults.LSConcurrentResponseHandler
-import com.jetbrains.ls.api.features.utils.traceProvider
+import com.jetbrains.ls.api.features.utils.noOpTraceProvider
 import com.jetbrains.lsp.implementation.LspHandlerContext
 import com.jetbrains.lsp.protocol.DefinitionParams
 import com.jetbrains.lsp.protocol.Location
 
 object LSDefinition {
-    val scope: Scope = Scope("lsp.definition")
-    private val tracer = TelemetryManager.getTracer(scope)
 
     context(server: LSServer, configuration: LSConfiguration, handlerContext: LspHandlerContext)
     suspend fun getDefinition(params: DefinitionParams): List<Location> {
@@ -22,7 +18,7 @@ object LSDefinition {
             resultSerializer = Location.serializer(),
             providers = configuration.entriesFor<LSDefinitionProvider>(params.textDocument),
             getResults = { definitionProvider ->
-                tracer.traceProvider(
+                noOpTraceProvider(
                     spanName = "provider.definition",
                     provider = definitionProvider,
                     resultsFlow = definitionProvider.provideDefinitions(params),
